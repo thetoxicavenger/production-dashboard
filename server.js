e9399acf503f10347c47c6f90f2dff0608588177
Mitchell Cravens <mitch@fjld.co>
2016-06-30 08:36:18 -0700

    Init commit.

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+'use strict';
+
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    compression = require('compression');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(compression());
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
+console.log("App listening on Port 3000!");
